746bf02cc044d9b36914678b34940f82977b166c
mydltmf123 <21400541@handong.edu>
2017-09-08 22:36:20 +0900

    bootstrap

--- jobsnepal/src/app/app.module.ts
+++ jobsnepal/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent }        from './app.component';
 import { MenuDetailComponent } from './menu-detail.component';
@@ -8,7 +9,8 @@ import { MenuDetailComponent } from './menu-detail.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AlertModule.forRoot()
   ],
   declarations: [
     AppComponent,
